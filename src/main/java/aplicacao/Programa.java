ff91832dd31acf7b13ade1eb985d47039ffc687e
DenysFerreira <lerroandeco@gmail.com>
2020-03-17 22:37:24 -0300

    Read (find) - JPA

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,19 +10,15 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+
 		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 
 	}
 
